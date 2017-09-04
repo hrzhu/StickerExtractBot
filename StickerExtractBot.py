8a662ef760f6c6eba86641e09714275f1be88339
Hairong Zhu <a@hrzhu.me>
2017-09-04 15:21:38 +0800

    init commit

---
+++ StickerExtractBot.py
@@ -0,0 +1,44 @@
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext.dispatcher import run_async
+
+from PIL import Image
+from io import BytesIO
+import requests
+import logging
+
+from config import token
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+updater = Updater(token=token)
+dispatcher = updater.dispatcher
+
+@run_async
+def sticker(bot, update):
+    res = requests.get(bot.get_file(file_id=update.message.sticker.file_id).file_path)
+    img = Image.open(BytesIO(res.content))
+    compressed_image = BytesIO()
+    img.save(compressed_image, 'PNG')
+    compressed_image.seek(0)
+
+    bot.send_document(chat_id=update.message.chat_id,
+                      document=compressed_image,
+                      filename=update.message.sticker.file_id + '.png',
+                      reply_to_message_id=update.message.message_id,
+                      disable_notification=True,
+                      timeout=60)
+
+    compressed_image.seek(0)
+
+    bot.send_photo(chat_id=update.message.chat_id,
+                   photo=compressed_image,
+                   reply_to_message_id=update.message.message_id,
+                   disable_notification=True)
+
+
+sticker_handler = MessageHandler(Filters.sticker, sticker)
+dispatcher.add_handler(sticker_handler)
+
+updater.start_polling()
